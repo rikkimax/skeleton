dc9521997ffcbbb9fd0b77c92713a00192d93c59
rikki cattermole <pbdadmin@gmail.com>
2014-07-14 19:37:00 +1200

    Non tested, but bitbucket provider should work.

---
+++ source/skeleton/providers/bitbucket.d
@@ -0,0 +1,97 @@
+﻿module skeleton.providers.bitbucket;
+import skeleton.providers.defs;
+
+class BitbucketProvider : IProvider {
+	bool canUse(string repo) {
+		return mainFile(repo) !is null;
+	}
+
+	string mainFile(string repo) {
+		string temp;
+
+		temp = cast(string)downloadRepoFile(repo, "skeleton.txt");
+		if (temp !is null)
+			return temp;
+
+		// other formats here
+
+		return null;
+	}
+
+	ubyte[] downloadRepoFile(string repo, string file) {
+		ubyte[] temp;
+
+		if (encodeBitbucketURL(repo, false) !is null) {
+			temp = downloadFile(encodeBitbucketURL(repo), file);
+			if (temp !is null)
+				return temp;
+		}
+
+		if (encodeBitbucketURL(repo, true) !is null) {
+			temp = downloadFile(encodeBitbucketURL(repo, true), file);
+			if (temp !is null)
+				return temp;
+		}
+
+		// other formats here
+
+		return null;
+	}
+
+	bool hasFile(string repo, string file) {
+		return downloadFile(repo, file) !is null;
+	}
+}
+
+protected {
+	import skeleton.util : split;
+
+	pure string encodeBitbucketURL(string text, bool isGit=false) {
+		string[] temp;
+
+		string repo;
+		string user;
+		string branch;
+
+		temp = split(text, "@");
+		if (temp.length != 2)
+			return null;
+		repo = temp[0];
+
+		temp = split(temp[1], "#");
+		if (temp.length == 2) {
+			user = temp[0];
+			string temp2 = temp[1];
+			temp = temp[1].split("/");
+			branch = temp[0];
+			temp[0] = temp2[branch.length + 1 .. $];
+		} else if (temp.length == 1) {
+			string temp2 = temp[0];
+			temp = temp[0].split("/");
+			user = temp[0];
+
+			if (temp2.length <= user.length)
+				return null;
+
+			temp[0] = temp2[user.length + 1 .. $];
+
+			if (isGit)
+				branch = "master";
+			else
+				branch = "tip";
+		} else
+			return null;
+
+		return "https://bitbucket.org/" ~ user ~ "/" ~ repo ~ "/raw/" ~ branch ~ "/" ~ temp[0];
+	}
+
+	unittest {
+		assert(encodeBitbucketURL("skeleton@rikkimax#master/examples/basic") == "https://bitbucket.org/rikkimax/skeleton/raw/master/examples/basic");
+		assert(encodeBitbucketURL("skeleton@rikkimax/examples/basic") == "https://bitbucket.org/rikkimax/skeleton/raw/tip/examples/basic");
+		assert(encodeBitbucketURL("skeleton@rikkimax/examples/basic", true) == "https://bitbucket.org/rikkimax/skeleton/raw/master/examples/basic");
+	}
+}
+
+static this() {
+	registerProvider(new BitbucketProvider);
+}
